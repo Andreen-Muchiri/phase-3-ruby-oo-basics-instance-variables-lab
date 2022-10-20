41ff23156f3ba7d7a482418c15996d4e43a576c9
Andreen-Muchiri <andreenshii@gmail.com>
2022-10-20 15:18:46 +0300

    Complete assignment

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,3 +1,14 @@
 class Dog
+# set name of the dog
+    def name=(dog_name)
+    @this_dogs_name = dog_name
+    end
+# instance method to get the name of the dog
+def name
+    @this_dogs_name
 
 end
+end
+lassie = Dog.new
+lassie.name = "Lassie"
+puts lassie.name
